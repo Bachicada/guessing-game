f3200f31356aedab51afc2d75125a351089b9a51
Bachicada <bachicada@gmail.com>
2021-09-04 14:31:06 +0300

    docs:done game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min=min;
+      this.max=max;
     }
 
+    //returns solution candidate (you make an assumption based on range and previous assumptions)
     guess() {
-
+    this.answear = Math.round(this.min+(this.max - this.min) / 2);
+    return this.answear;
     }
 
+    //is called if prev call of guess() returned number which is greater than answer
     lower() {
-
+     this.max=this.answear;
     }
 
+    //is called if prev call of guess() returned number which is lower than answer
     greater() {
-
+        this.min=this.answear;
     }
 }
 
